cf4376c29d2838bced227e9203b1891ba4d33647
Saimon8420 <latifulkabir567@gmail.com>
2024-01-08 15:54:03 +0600

    some bug fixed

--- src/components/Todos/Todo.jsx
+++ src/components/Todos/Todo.jsx
@@ -46,7 +46,7 @@ const Todo = () => {
                                 <div className="grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6 sm:mb-5">
                                     <div className="sm:col-span-4">
                                         <label htmlFor="search" className="block text-sm font-medium leading-6 text-gray-900 text-left">
-                                            Search Value
+                                            Search todo by [Title/Details]
                                         </label>
                                         <div className="mt-2">
                                             <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
